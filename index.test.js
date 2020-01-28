512dddb45ab88ebbda6e858d966e9785768daf70
apurva1611 <mathurapurva16@gmail.com>
2020-01-28 05:29:26 -0500

    up

--- index.test.js
+++ index.test.js
@@ -6,3 +6,6 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(2, 2)).toBe(4);
 });
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(6, 2)).toBe(8);
+});
